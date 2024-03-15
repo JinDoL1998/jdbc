73d8206fee7a8713eae05881e3ce405b3c4e2846
JinDoL1998 <jkhgfdds1@gmail.com>
2024-03-15 17:49:16 +0900

    jdbc first commit

---
+++ jdbcPro/src/domain/EmpVO.java
@@ -0,0 +1,28 @@
+package domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmpVO {
+
+	
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private String hiredate;
+	private int sal;
+	private int comm;
+	private int deptno;
+	
+}
